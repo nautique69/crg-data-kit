3a163adf97455439f8cb8fe24875432f9bf07a54
Daniel Errington <derrington@gmail.com>
2025-03-18 18:44:17 +0000

    Add penalty display setup script

--- scripts/configure-penaltydisplay.sh
+++ scripts/configure-penaltydisplay.sh
@@ -6,7 +6,7 @@ sudo raspi-config nonint do_wayland W1
 sudo apt-get update
 sudo apt-get install -y unclutter xdotool
 
-cat > run-scoredisplay.sh <<EOF
+cat > run-penaltydisplay.sh <<EOF
 #!/bin/bash
 
 xset s noblank
@@ -18,11 +18,11 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type": "Crashed"/"exit_type": "Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-DISPLAY=:0.0 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --no-sandbox --kiosk http://scoreboard.local/views/standard/
+DISPLAY=:0.0 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --no-sandbox --kiosk http://scoreboard.local/views/wb/?
 EOF
-chmod +x ./run-scoredisplay.sh
+chmod +x ./run-penaltydisplay.sh
 
 mkdir -p /home/pi/.config/lxsession/LXDE-pi/
-echo "/home/pi/run-scoredisplay.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "/home/pi/run-penaltydisplay.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
 sudo reboot
