567b2dff925887c0efc6f405cd43e43007127fc4
nautique69 <dave@almond.me.uk>
2025-07-12 15:35:05 +0100

    Update configure-scoreboard.sh

--- scripts/configure-scoreboard.sh
+++ scripts/configure-scoreboard.sh
@@ -11,24 +11,25 @@ if [ -z "$1" ]
  echo "Getting latest version of CRG"
  CRG_VERSION="$(curl -s https://api.github.com/repos/rollerderby/scoreboard/releases/latest | jq -r '.tag_name')"
 else
+ echo "Using passed version"
  CRG_VERSION=$1
 fi
 
 echo "Installing $CRG_VERSION :)"
 
-mkdir ~/scoreboard
-cd scoreboard
-wget https://github.com/rollerderby/scoreboard/releases/download/$CRG_VERSION/crg-scoreboard_$CRG_VERSION.zip
-unzip ./crg-scoreboard_$CRG_VERSION.zip
-cd ..
+#mkdir ~/scoreboard
+#cd scoreboard
+#wget https://github.com/rollerderby/scoreboard/releases/download/$CRG_VERSION/crg-scoreboard_$CRG_VERSION.zip
+#unzip ./crg-scoreboard_$CRG_VERSION.zip
+#cd ..
 
-cat > run-scoreboard.sh <<EOF
-#! /bin/bash
-sudo /home/pi/scoreboard/crg-scoreboard_$CRG_VERSION/scoreboard.sh --port=80
-EOF
-chmod +x ./run-scoreboard.sh
+#cat > run-scoreboard.sh <<EOF
+##! /bin/bash
+#sudo /home/pi/scoreboard/crg-scoreboard_$CRG_VERSION/scoreboard.sh --port=80
+#EOF
+#chmod +x ./run-scoreboard.sh
 
-mkdir -p /home/pi/.config/lxsession/LXDE-pi/
-echo "/home/pi/run-scoreboard.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+#mkdir -p /home/pi/.config/lxsession/LXDE-pi/
+#echo "/home/pi/run-scoreboard.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
-sudo reboot
+#sudo reboot
