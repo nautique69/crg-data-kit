760ceb902986f2f4f45febb1f0363c8a3ec6711b
nautique69 <dave@almond.me.uk>
2025-07-12 15:44:17 +0100

    Update configure-scoreboard.sh

--- scripts/configure-scoreboard.sh
+++ scripts/configure-scoreboard.sh
@@ -17,19 +17,19 @@ fi
 
 echo "Installing $CRG_VERSION :)"
 
-#mkdir ~/scoreboard
-#cd scoreboard
-#wget https://github.com/rollerderby/scoreboard/releases/download/$CRG_VERSION/crg-scoreboard_$CRG_VERSION.zip
-#unzip ./crg-scoreboard_$CRG_VERSION.zip
-#cd ..
+mkdir ~/scoreboard
+cd scoreboard
+wget https://github.com/rollerderby/scoreboard/releases/download/$CRG_VERSION/crg-scoreboard_$CRG_VERSION.zip
+unzip ./crg-scoreboard_$CRG_VERSION.zip
+cd ..
 
-#cat > run-scoreboard.sh <<EOF
-##! /bin/bash
-#sudo /home/pi/scoreboard/crg-scoreboard_$CRG_VERSION/scoreboard.sh --port=80
-#EOF
-#chmod +x ./run-scoreboard.sh
+cat > run-scoreboard.sh <<EOF
+#! /bin/bash
+sudo /home/pi/scoreboard/crg-scoreboard_$CRG_VERSION/scoreboard.sh --port=80
+EOF
+chmod +x ./run-scoreboard.sh
 
-#mkdir -p /home/pi/.config/lxsession/LXDE-pi/
-#echo "/home/pi/run-scoreboard.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+mkdir -p /home/pi/.config/lxsession/LXDE-pi/
+echo "/home/pi/run-scoreboard.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
-#sudo reboot
+sudo reboot
